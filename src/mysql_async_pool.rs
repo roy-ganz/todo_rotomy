e43d0bf1324435e23ba223eb0d1857b19cb72e46
roy-ganz <roy.ganz@glanis.biz>
2021-10-15 18:27:35 +0200

    fmt

--- src/mysql_async_pool.rs
+++ src/mysql_async_pool.rs
@@ -1,8 +1,7 @@
-use toql_mysql_async::mysql_async::{Pool, Conn, Error};
-use rocket_db_pools::{Database, Config};
 use rocket::figment::Figment;
+use rocket_db_pools::{Config, Database};
 use std::borrow::Cow;
-
+use toql_mysql_async::mysql_async::{Conn, Error, Pool};
 
 pub struct MySqlAsyncPool(Pool);
 
@@ -12,15 +11,16 @@ impl rocket_db_pools::Pool for MySqlAsyncPool {
     type Error = Error;
 
     async fn init(figment: &Figment) -> Result<Self, Self::Error> {
-        let config: Config = figment.extract().map_err(|e|Error::Other(Cow::Owned(e.to_string())))?;
-        
+        let config: Config = figment
+            .extract()
+            .map_err(|e| Error::Other(Cow::Owned(e.to_string())))?;
+
         let pool = MySqlAsyncPool(Pool::from_url(config.url)?);
 
         Ok(pool)
     }
 
-     async fn get(&self) -> Result<Self::Connection, Self::Error> {
-         self.0.get_conn().await
-     }
-
+    async fn get(&self) -> Result<Self::Connection, Self::Error> {
+        self.0.get_conn().await
+    }
 }
