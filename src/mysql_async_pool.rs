1e5761448e8122f5866531183768ccd784f95d1b
roy-ganz <roy.ganz@glanis.biz>
2021-10-15 18:26:31 +0200

    Initial

---
+++ src/mysql_async_pool.rs
@@ -0,0 +1,26 @@
+use toql_mysql_async::mysql_async::{Pool, Conn, Error};
+use rocket_db_pools::{Database, Config};
+use rocket::figment::Figment;
+use std::borrow::Cow;
+
+
+pub struct MySqlAsyncPool(Pool);
+
+#[rocket::async_trait]
+impl rocket_db_pools::Pool for MySqlAsyncPool {
+    type Connection = Conn;
+    type Error = Error;
+
+    async fn init(figment: &Figment) -> Result<Self, Self::Error> {
+        let config: Config = figment.extract().map_err(|e|Error::Other(Cow::Owned(e.to_string())))?;
+        
+        let pool = MySqlAsyncPool(Pool::from_url(config.url)?);
+
+        Ok(pool)
+    }
+
+     async fn get(&self) -> Result<Self::Connection, Self::Error> {
+         self.0.get_conn().await
+     }
+
+}
